36f5582d51e7924d21639381ae4241810f258f27
NguyenHuuNguyen <80516150+NguyenHuuNguyen@users.noreply.github.com>
2021-05-18 01:02:19 +0700

    Add files via upload

---
+++ PBL3_NetManagement/PBL3_NetManagement/BLL/BLL_NM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3_NetManagement.BLL
+{
+    class BLL_NM
+    {
+        private static BLL_NM _Instance;
+        public static BLL_NM Instance
+        {
+            get
+            {
+                if (_Instance == null) _Instance = new BLL_NM();
+                return _Instance;
+            }
+            private set { }
+        }
+        public string Get_idComputer()
+        {
+            string username = Dns.GetHostName();
+            IPHostEntry Ip = new IPHostEntry();
+            Ip = Dns.GetHostEntry(username);
+            return Ip.AddressList[Ip.AddressList.Length - 1].ToString();
+        }
+    }
+}
---
+++ PBL3_NetManagement/PBL3_NetManagement/GUI/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PBL3_NetManagement.BLL;
+
+namespace PBL3_NetManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = BLL_NM.Instance.Get_idComputer();
+            NetManagementEntities db = new NetManagementEntities();
+            dataGridView1.DataSource = db.Accounts.ToList();
+        }
+    }
+}
