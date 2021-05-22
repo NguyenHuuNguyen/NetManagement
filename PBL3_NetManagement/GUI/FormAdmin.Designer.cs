d73709320e8502525604670e3bfe2695017c2ede
TranThiThanhNga <tranthithanhnga913@gmail.com>
2021-05-22 19:32:55 +0700

    admin UI

---
+++ PBL3_NetManagement/GUI/FormAdmin.Designer.cs
@@ -0,0 +1,365 @@
+﻿
+namespace PBL3_NetManagement
+{
+    partial class FormAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Computers = new System.Windows.Forms.TabPage();
+            this.Accouts = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Goods = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.SystemLog = new System.Windows.Forms.TabPage();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button10 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tabControl1.SuspendLayout();
+            this.Accouts.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Goods.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SystemLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Computers);
+            this.tabControl1.Controls.Add(this.Accouts);
+            this.tabControl1.Controls.Add(this.Goods);
+            this.tabControl1.Controls.Add(this.SystemLog);
+            this.tabControl1.Location = new System.Drawing.Point(-1, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1174, 675);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Computers
+            // 
+            this.Computers.BackgroundImage = global::PBL3_NetManagement.Properties.Resources.Backgroud;
+            this.Computers.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Computers.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Computers.Location = new System.Drawing.Point(4, 25);
+            this.Computers.Name = "Computers";
+            this.Computers.Padding = new System.Windows.Forms.Padding(3);
+            this.Computers.Size = new System.Drawing.Size(1166, 646);
+            this.Computers.TabIndex = 0;
+            this.Computers.Text = "Computers";
+            this.Computers.UseVisualStyleBackColor = true;
+            // 
+            // Accouts
+            // 
+            this.Accouts.BackgroundImage = global::PBL3_NetManagement.Properties.Resources.Backgroud;
+            this.Accouts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Accouts.Controls.Add(this.button10);
+            this.Accouts.Controls.Add(this.button4);
+            this.Accouts.Controls.Add(this.textBox1);
+            this.Accouts.Controls.Add(this.dataGridView1);
+            this.Accouts.Controls.Add(this.button3);
+            this.Accouts.Controls.Add(this.button2);
+            this.Accouts.Controls.Add(this.button1);
+            this.Accouts.Location = new System.Drawing.Point(4, 25);
+            this.Accouts.Name = "Accouts";
+            this.Accouts.Padding = new System.Windows.Forms.Padding(3);
+            this.Accouts.Size = new System.Drawing.Size(1166, 646);
+            this.Accouts.TabIndex = 1;
+            this.Accouts.Text = "Account";
+            this.Accouts.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button4.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(833, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 47);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(566, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 30);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(888, 471);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(300, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(172, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(45, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Goods
+            // 
+            this.Goods.BackgroundImage = global::PBL3_NetManagement.Properties.Resources.Backgroud;
+            this.Goods.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Goods.Controls.Add(this.button8);
+            this.Goods.Controls.Add(this.textBox2);
+            this.Goods.Controls.Add(this.button7);
+            this.Goods.Controls.Add(this.button6);
+            this.Goods.Controls.Add(this.button5);
+            this.Goods.Controls.Add(this.dataGridView2);
+            this.Goods.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Goods.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Goods.Location = new System.Drawing.Point(4, 25);
+            this.Goods.Name = "Goods";
+            this.Goods.Size = new System.Drawing.Size(1166, 646);
+            this.Goods.TabIndex = 3;
+            this.Goods.Text = "Goods";
+            this.Goods.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button8.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(827, 35);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 44);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Search";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(459, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button7.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(308, 35);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 44);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Delete";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button6.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(173, 35);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 44);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Edit";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button5.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(41, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 44);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Add";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(41, 114);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(886, 480);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // SystemLog
+            // 
+            this.SystemLog.BackgroundImage = global::PBL3_NetManagement.Properties.Resources.Backgroud;
+            this.SystemLog.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SystemLog.Controls.Add(this.listView1);
+            this.SystemLog.Controls.Add(this.button9);
+            this.SystemLog.Controls.Add(this.textBox3);
+            this.SystemLog.Location = new System.Drawing.Point(4, 25);
+            this.SystemLog.Name = "SystemLog";
+            this.SystemLog.Size = new System.Drawing.Size(1166, 646);
+            this.SystemLog.TabIndex = 2;
+            this.SystemLog.Text = "System Log";
+            this.SystemLog.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button9.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button9.Location = new System.Drawing.Point(442, 31);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 44);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Search";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(54, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(347, 30);
+            this.textBox3.TabIndex = 1;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button10.Font = new System.Drawing.Font("MTO Chaney", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Location = new System.Drawing.Point(427, 33);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 46);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "Add Bill";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(54, 119);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(869, 475);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PBL3_NetManagement.Properties.Resources.Backgroud;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(979, 654);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "FormAdmin";
+            this.Text = "NetManagement";
+            this.tabControl1.ResumeLayout(false);
+            this.Accouts.ResumeLayout(false);
+            this.Accouts.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Goods.ResumeLayout(false);
+            this.Goods.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.SystemLog.ResumeLayout(false);
+            this.SystemLog.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Computers;
+        private System.Windows.Forms.TabPage Accouts;
+        private System.Windows.Forms.TabPage Goods;
+        private System.Windows.Forms.TabPage SystemLog;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
